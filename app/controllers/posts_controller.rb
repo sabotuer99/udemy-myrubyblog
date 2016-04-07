bdf4af9265fdfb874b0bf0dfa03ed82d750567ea
Troy Whorten <sabotuer99@gmail.com>
2016-04-07 16:16:45 +0000

    added models and views for posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+    def index
+        @content_first = 'This is some sample text for our awesome new Ruby blog'
+        @content_second = 'Second post'
+    end
+    
+    def new
+    end
+    
+    def create
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def show 
+    end
+    
+    def destroy
+    end
+    
+end
